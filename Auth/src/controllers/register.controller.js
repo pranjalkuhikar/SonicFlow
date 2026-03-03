00660a7c7dbb871bef70ed0bcdeddb111d5fc1d3
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-03-03 12:41:54 +0530

    fix(register): add validation for required fields
    Add check for missing username, email, or password in registration endpoint to prevent incomplete submissions and ensure proper error handling.

--- Auth/src/controllers/register.controller.js
+++ Auth/src/controllers/register.controller.js
@@ -3,14 +3,21 @@ import User from "../models/register.model.js";
 export const register = async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
     const userExits = await User.findOne({ email });
     if (userExits) {
       return res.status(400).json({ message: "Email already exists" });
     }
+
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
+
     const user = new User({ username, email, password: hashPassword });
     await user.save();
+
     res.status(201).json({
       message: "User created successfully",
       user,
