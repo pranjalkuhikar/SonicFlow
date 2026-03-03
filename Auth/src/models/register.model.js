968f64f92144c9528957efc4830b3e92fac8b51a
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-03-03 09:14:48 +0530

    feat: create user model with schema for registration

---
+++ Auth/src/models/register.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timeStamp: true,
+  },
+);
+
+const User = userSchema.model("User", userSchema);
+
+export default User;
