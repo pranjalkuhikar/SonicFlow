0e45793bab794db220ee352fa0187560f2da604f
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2026-03-04 11:22:53 +0530

    feat(email): integrate nodemailer for email sending functionality

--- Notification/src/configs/config.js
+++ Notification/src/configs/config.js
@@ -5,6 +5,12 @@ dotenv.config();
 const _config = {
   PORT: process.env.PORT,
   MONGO_URL: process.env.MONGO_URL,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  REFRESH_TOKEN: process.env.REFRESH_TOKEN,
+  EMAIL_USER: process.env.EMAIL_USER,
+  JWT_SECRET: process.env.JWT_SECRET,
+  RABBITMQ_URI: process.env.RABBITMQ_URI,
 };
 
 const config = Object.freeze(_config);
---
+++ Notification/src/utils/email.js
@@ -0,0 +1,38 @@
+import nodemailer from "nodemailer";
+import config from "../configs/config.js";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    type: "OAuth2",
+    user: config.EMAIL_USER,
+    clientId: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+    refreshToken: config.REFRESH_TOKEN,
+  },
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("Error connecting to email server:", error);
+  } else {
+    console.log("Email server is ready to send messages");
+  }
+});
+
+const sendEmail = async (to, subject, text, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Spotify Piper" <${config.EMAIL_USER}>`, // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text, // plain text body
+      html, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
